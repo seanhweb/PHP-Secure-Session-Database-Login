dd6a69d289c183d83e9f597a1bd896312bddb019
Sean Houlihan <sean@seanhweb.com>
2016-06-02 13:33:18 -0700

    Config File

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	define("DB_HOST", "localhost");
+	define("DB_NAME", "equipment");
+	define("DB_USER", "noob");
+	define("DB_PASS", "hackme");
+	define("WEB_ROOT", "http://myserver.com");
+	define("SESSION_DURATION", 60); 
+?>
